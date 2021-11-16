8aea7fc94b21f3d8651326228a6e664cc3039dee
nascvi <vaiiho.nascimbene@gmail.com>
2021-11-16 18:13:58 +0100

    h1 initil

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>The Collectors Hang Out</h1>
+	  <img src={logo} className="App-logo" alt="logo" />
         <p>
           what's up doc collectors?!
         </p>
